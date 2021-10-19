0bc693d77375acededefc9b6444977ff28774d62
nhirokinet <nhirokinet@nhiroki.net>
2021-10-20 02:13:29 +0900

    Add database tests as connectedcheck

--- app/src/androidTest/java/net/nhiroki/bluelineconsole/dataStore/persistent/oldVersions/HomeScreenSetting_1_2_5.java
+++ app/src/androidTest/java/net/nhiroki/bluelineconsole/dataStore/persistent/oldVersions/HomeScreenSetting_1_2_5.java
@@ -1,10 +1,12 @@
-package net.nhiroki.bluelineconsole.dataStore.persistent;
+package net.nhiroki.bluelineconsole.dataStore.persistent.oldVersions;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import net.nhiroki.bluelineconsole.dataStore.persistent.HomeScreenSetting;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,32 +15,22 @@ import java.util.List;
 /**
  * Database for home screen setting, but this does not include widget info, due to backup treatment.
  */
-public class HomeScreenSetting extends SQLiteOpenHelper {
+public class HomeScreenSetting_1_2_5 extends SQLiteOpenHelper {
     public static final int HOME_SCREEN_TYPE_COMMAND = 1;
-
-    public static class HomeScreenDefaultItem {
-        public int id;
-        public int type;
-        public String data;
-    }
 
     private static final String DATABASE_NAME = "home_screen_setting.sqlite";
     private static final int DATABASE_VERSION = 1;
 
-    private static HomeScreenSetting _singleton = null;
+    private static HomeScreenSetting_1_2_5 _singleton = null;
 
-    public synchronized static HomeScreenSetting getInstance(Context context) {
+    public synchronized static HomeScreenSetting_1_2_5 getInstance(Context context) {
         if (_singleton == null) {
-            _singleton = new HomeScreenSetting(context);
+            _singleton = new HomeScreenSetting_1_2_5(context);
         }
         return _singleton;
     }
 
-    public static void destroyFilesForCleanTest(Context context) {
-        context.getDatabasePath(DATABASE_NAME).delete();
-    }
-
-    private HomeScreenSetting(Context context) {
+    private HomeScreenSetting_1_2_5(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -46,10 +38,10 @@ public class HomeScreenSetting extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(
                 "CREATE TABLE home_screen_default_items (" +
-                "  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                "  type INTEGER NOT NULL," +
-                "  data TEXT NOT NULL" +
-                ")");
+                        "  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                        "  type INTEGER NOT NULL," +
+                        "  data TEXT NOT NULL" +
+                        ")");
     }
 
     @Override
@@ -63,7 +55,7 @@ public class HomeScreenSetting extends SQLiteOpenHelper {
         super.close();
     }
 
-    public long addHomeScreenDefaultItem(HomeScreenDefaultItem homeScreenDefaultItem) {
+    public long addHomeScreenDefaultItem(HomeScreenSetting.HomeScreenDefaultItem homeScreenDefaultItem) {
         ContentValues cv = new ContentValues();
         cv.put("type", homeScreenDefaultItem.type);
         cv.put("data", homeScreenDefaultItem.data);
@@ -71,13 +63,13 @@ public class HomeScreenSetting extends SQLiteOpenHelper {
         return this.getWritableDatabase().insert("home_screen_default_items", null, cv);
     }
 
-    public List<HomeScreenDefaultItem> getAllHomeScreenDefaultItems() {
-        List<HomeScreenDefaultItem> ret = new ArrayList<>();
+    public List<HomeScreenSetting.HomeScreenDefaultItem> getAllHomeScreenDefaultItems() {
+        List<HomeScreenSetting.HomeScreenDefaultItem> ret = new ArrayList<>();
 
         Cursor curEntry = this.getReadableDatabase().query("home_screen_default_items", new String[]{"id", "type", "data"}, null, null, null, null, null);
 
         while (curEntry.moveToNext()) {
-            HomeScreenDefaultItem e = new HomeScreenDefaultItem();
+            HomeScreenSetting.HomeScreenDefaultItem e = new HomeScreenSetting.HomeScreenDefaultItem();
             e.id = curEntry.getInt(curEntry.getColumnIndex("id"));
             e.type = curEntry.getInt(curEntry.getColumnIndex("type"));
             e.data = curEntry.getString(curEntry.getColumnIndex("data"));
@@ -90,12 +82,12 @@ public class HomeScreenSetting extends SQLiteOpenHelper {
         return ret;
     }
 
-    public HomeScreenDefaultItem getHomeScreenDefaultItemById(int id) {
+    public HomeScreenSetting.HomeScreenDefaultItem getHomeScreenDefaultItemById(int id) {
         Cursor curEntry = this.getReadableDatabase().query("home_screen_default_items", new String[]{"id", "type", "data"},
                 "id = ?", new String[]{String.valueOf(id)}, null, null, null);
 
         if (curEntry.moveToNext()) {
-            HomeScreenDefaultItem e = new HomeScreenDefaultItem();
+            HomeScreenSetting.HomeScreenDefaultItem e = new HomeScreenSetting.HomeScreenDefaultItem();
             e.id = curEntry.getInt(curEntry.getColumnIndex("id"));
             e.type = curEntry.getInt(curEntry.getColumnIndex("type"));
             e.data = curEntry.getString(curEntry.getColumnIndex("data"));
@@ -115,7 +107,7 @@ public class HomeScreenSetting extends SQLiteOpenHelper {
         this.getWritableDatabase().delete("home_screen_default_items", "id = ?", args);
     }
 
-    public void updateHomeScreenDefaultItem(HomeScreenDefaultItem entry) {
+    public void updateHomeScreenDefaultItem(HomeScreenSetting.HomeScreenDefaultItem entry) {
         String[] args = new String[1];
         args[0] = String.valueOf(entry.id);
 
