ce0ad6c6cfb07d49fc16130eca4447b71d4814e3
nhirokinet <nhirokinet@nhiroki.net>
2021-10-31 01:34:11 +0900

    Add compatibility test for WidgetSetting with v1.2.8

--- app/src/androidTest/java/net/nhiroki/bluelineconsole/dataStore/deviceLocal/WidgetSettingTest.java
+++ app/src/androidTest/java/net/nhiroki/bluelineconsole/dataStore/deviceLocal/WidgetSettingTest.java
@@ -5,7 +5,7 @@ import android.test.AndroidTestCase;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
-import net.nhiroki.bluelineconsole.dataStore.deviceLocal.oldVersions.WidgetsSetting_1_2_5;
+import net.nhiroki.bluelineconsole.dataStore.deviceLocal.oldVersions.*;
 import net.nhiroki.bluelineconsole.wrapperForAndroid.AppWidgetsHostManager;
 
 import org.junit.Before;
@@ -107,6 +107,53 @@ public class WidgetSettingTest extends AndroidTestCase {
 
         // Introduced in 1.2.8
         testHomeScreenWidgets.get(0).afterDefaultItem = -1;
+
+        entriesLoadTest(testCommands, testHomeScreenWidgets);
+    }
+
+    @Test
+    public void widgetSettingUpgradeFrom_1_2_8_Test() {
+        WidgetsSetting.destroyFilesForCleanTest(this.getContext());
+
+        WidgetsSetting_1_2_8 oldVersionInstance = WidgetsSetting_1_2_8.getInstance(this.getContext());
+
+        List<AppWidgetsHostManager.WidgetCommand> testCommands = new ArrayList<>();
+
+        testCommands.add(new AppWidgetsHostManager.WidgetCommand(0, null, 123));
+        testCommands.get(0).command = "test";
+        testCommands.get(0).heightPx = 1234;
+
+        testCommands.add(new AppWidgetsHostManager.WidgetCommand(0, null, 124));
+        testCommands.get(1).command = "test2";
+        testCommands.get(1).heightPx = 1236;
+
+        testCommands.add(new AppWidgetsHostManager.WidgetCommand(0, null, 125));
+        testCommands.get(2).command = "test";
+        testCommands.get(2).heightPx = 1238;
+
+        testCommands.add(new AppWidgetsHostManager.WidgetCommand(0, null, 123));
+        testCommands.get(3).command = "test";
+        testCommands.get(3).heightPx = 1238;
+
+        for (AppWidgetsHostManager.WidgetCommand c: testCommands) {
+            oldVersionInstance.addWidgetCommand(c);
+        }
+
+        List<AppWidgetsHostManager.HomeScreenWidgetInfo> testHomeScreenWidgets = new ArrayList<>();
+
+        testHomeScreenWidgets.add(new AppWidgetsHostManager.HomeScreenWidgetInfo(0, null, 123));
+        testHomeScreenWidgets.get(0).heightPx = 1234;
+        testHomeScreenWidgets.get(0).afterDefaultItem = -1;
+
+        testHomeScreenWidgets.add(new AppWidgetsHostManager.HomeScreenWidgetInfo(0, null, 123));
+        testHomeScreenWidgets.get(1).heightPx = 1238;
+        testHomeScreenWidgets.get(1).afterDefaultItem = 123;
+
+        for (AppWidgetsHostManager.HomeScreenWidgetInfo w: testHomeScreenWidgets) {
+            oldVersionInstance.addWidgetToHomeScreen(w);
+        }
+
+        oldVersionInstance.close();
 
         entriesLoadTest(testCommands, testHomeScreenWidgets);
     }
