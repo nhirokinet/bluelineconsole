ca9cab4f10cd84de5def3e05dfefb84b461f6b29
nhirokinet <nhirokinet@nhiroki.net>
2021-11-09 20:53:59 +0900

    Misc code cleanup without changing behavior

--- app/src/main/java/net/nhiroki/bluelineconsole/applicationMain/HomeActivity.java
+++ app/src/main/java/net/nhiroki/bluelineconsole/applicationMain/HomeActivity.java
@@ -1,10 +1,8 @@
 package net.nhiroki.bluelineconsole.applicationMain;
 
-import android.annotation.SuppressLint;
-
 public class HomeActivity extends MainActivity {
     public HomeActivity() {
         super();
-        this._iAmHomeActivity = true;
+        this.iAmHomeActivity = true;
     }
 }
