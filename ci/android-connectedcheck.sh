68976be0853a0ddf810ba5392588333ba562d027
nhirokinet <nhirokinet@nhiroki.net>
2025-01-06 19:04:10 +0900

    Use android-34 image in GitLab CI

--- ci/android-connectedcheck.sh
+++ ci/android-connectedcheck.sh
@@ -1,10 +1,34 @@
 #!/bin/bash -eux
 
-# x86 image is not provided for 30 or later, but at least on my end x86_64 image could not run on emulator command
 # Minimum supported version: SDK22
 # In current code, non-English test runs on SDK >=24
 
-for sdkver in 29 24 22
+for sdkver in 34
+do
+    sdkmanager "system-images;android-$sdkver;default;x86_64"
+    echo no | avdmanager create avd -f -n vm$sdkver -k "system-images;android-$sdkver;default;x86_64"
+    ${ANDROID_HOME}/emulator/emulator -avd vm$sdkver -no-window -no-audio &
+    EMULATOR_PID=$!
+    for i in $(seq 60); do if adb -e shell getprop init.svc.bootanim ; then break ; fi; sleep 5; done
+    adb -e shell getprop init.svc.bootanim
+    for i in $(seq 60); do if adb -e shell getprop init.svc.bootanim | grep stopped ; then break ; fi; sleep 5; done
+    adb -e shell getprop init.svc.bootanim | grep stopped
+    for i in $(seq 60); do if adb -e shell pm list packages ; then break ; fi; sleep 5; done
+    adb -e shell pm list packages
+    for i in $(seq 10); do if adb -e shell input keyevent 82 ; then break; fi; sleep 5; done  # unlock device
+    for i in $(seq 24); do if adb -e shell dumpsys power | grep mHoldingWakeLockSuspendBlocker=true ; then break; fi; sleep 5; done
+    sleep 20
+    # https://stackoverflow.com/questions/39457305/android-testing-waited-for-the-root-of-the-view-hierarchy-to-have-window-focus
+    adb shell am broadcast -a android.intent.action.CLOSE_SYSTEM_DIALOGS
+    sleep 10
+    ./gradlew connectedCheck || (sleep 30 && ./gradlew connectedCheck)
+    kill $EMULATOR_PID
+    while test -e /proc/$EMULATOR_PID; do sleep 1; done
+    avdmanager delete avd -n vm$sdkver
+    sdkmanager --uninstall "system-images;android-$sdkver;default;x86_64"
+done
+
+for sdkver in 24 22
 do
     sdkmanager "system-images;android-$sdkver;default;x86"
     echo no | avdmanager create avd -f -n vm$sdkver -k "system-images;android-$sdkver;default;x86"
